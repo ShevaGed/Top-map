1c7daeb41eaa95a2779240be8713502969d08ab8
Oleksii Koval <alex@koval.kharkov.ua>
2026-05-03 20:01:43 +0300

    remove cljs-runtime and public stale files

--- map_ukv/public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { map_ukv.core.init(); } catch (e) { console.error("An error occurred when calling (map-ukv.core/init)"); console.error(e); }
